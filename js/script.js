07afec3eb32d8d208f187f92dc4cc228b3ea5685
Maxim <konstantinovmax@gmail.com>
2021-06-01 19:18:52 +0300

    feat: реализовать функционал добавления и удаления новых фильмов

--- js/script.js
+++ js/script.js
@@ -28,22 +28,67 @@ const promoAdv = document.querySelectorAll('.promo__adv img');
 const filmGenre = document.querySelector('.promo__genre');
 const promoBackground = document.querySelector('.promo__bg');
 const filmItems = document.querySelector('.promo__interactive-list');
+const form = document.querySelector('.add');
+const formInput = form.querySelector('.adding__input');
+const submitButton = form.querySelector('.submit-button');
 
-promoAdv.forEach((item) => {
-    item.remove();
-});
+function deleteAdv(promos) {
+    promos.forEach((item) => {
+        item.remove();
+    });
+}
 
-filmGenre.textContent = 'драма';
-promoBackground.style.cssText = 'background: url(../img/bg.jpg) center center/cover no-repeat';
+function makeChanges() {
+    filmGenre.textContent = 'драма';
+    promoBackground.style.cssText = 'background: url(../img/bg.jpg) center center/cover no-repeat';
+}
 
-filmItems.innerHTML = '';
+function sortArr(arr) {
+    arr.sort();
+}
 
-movieDB.movies.sort();
+function renderMovie(films, parent) {
+    parent.innerHTML = '';
 
-movieDB.movies.forEach((item, i) => {
-    filmItems.innerHTML += `
-        <li class="promo__interactive-item">${i + 1}. ${item}
-            <div class="delete"></div>
-        </li>
-    `;
-});
+    films.forEach((item, i) => {
+        parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1}. ${item}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+
+    const deleteButtons = document.querySelectorAll('.delete');
+    deleteButtons.forEach((button, i) => {
+        button.addEventListener('click', () => {
+            button.parentElement.remove();
+            movieDB.movies.splice(i, 1);
+            renderMovie(films, parent);
+            sortArr(movieDB.movies);
+        });
+    });
+}
+
+function submitFormHandler(evt) {
+    evt.preventDefault();
+
+    let newFilm = formInput.value;
+
+    if (newFilm) {
+        if (newFilm.length > 21) {
+            newFilm = `${newFilm.substring(0, 22)}...`;
+        }
+
+        movieDB.movies.push(newFilm);
+        sortArr(movieDB.movies);
+        renderMovie(movieDB.movies, filmItems);
+    }
+    
+    evt.target.reset();
+}
+
+deleteAdv(promoAdv);
+makeChanges();
+sortArr(movieDB.movies);
+renderMovie(movieDB.movies, filmItems);
+form.addEventListener('submit', submitFormHandler);
